65e2f6f742e4c531003a4f52a0cc0cb33730f509
zton82005 <zton82005@gmail.com>
2023-07-21 22:21:13 +0800

    Updated na pod

--- components/CardCard.tsx
+++ components/CardCard.tsx
@@ -13,6 +13,8 @@ interface CarCardProps {
 const CardCard = ({ car }: CarCardProps ) => {
     const { city_mpg, year, make, model, transmission, drive } = car
 
+    const [isOpen, setIsOpen] = useState(false)
+
     const carRent = calculateCarRent(city_mpg, year)
 
   return (
@@ -35,6 +37,34 @@ const CardCard = ({ car }: CarCardProps ) => {
 
         <div className='relative w-full h-40 my-3 object-contain'>
             <Image src="/hero.png" alt='car model' fill priority className='object-contain' />
+        </div>
+
+        <div className='relative flex w-full mt-2'>
+            <div className='flex group-hover:invisible w-full justify-between text-gray'>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <Image src='/steering-wheel.svg' alt='steering wheel' width={20} height={20} />
+                    <p className='text-[14px]'>
+                        {transmission === 'a' ? 'Automatic' : 'Manual' }
+                    </p>
+                </div>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <Image src='/tire.svg' alt='tire' width={20} height={20} />
+                    <p className='text-[14px]'>
+                        {drive.toUpperCase()}
+                    </p>
+                </div>
+                <div className='flex flex-col justify-center items-center gap-2'>
+                    <Image src='/gas.svg' alt='tire' width={20} height={20} />
+                    <p className='text-[14px]'>
+                        {city_mpg} MPG
+                    </p>
+                </div>
+            </div>
+
+            <div className='car-card__btn-container'>
+                <CustomButton title="View More" containerStyles='w-full py-[16px] rounded-full bg-primary-blue' textStyles='text-white text-[14px] leading-[17px] font-bold' rightIcon='/right-arrow.svg' handleClick={() => setIsOpen(true)} />
+            </div>
+
         </div>
 
     </div>
--- types/index.ts
+++ types/index.ts
@@ -6,6 +6,9 @@ export interface CustomButtonProps {
     handleClick?:
     MouseEventHandler<HTMLButtonElement>
     btnType?: "button" | "submit"
+    textStyles?: string
+    rightIcon?: string
+    isDisable?: boolean
 }
 
 export interface searchManufacturerProps {
