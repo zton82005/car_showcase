e0a72808f543559b36aa76b7d75222d24bd64fce
zton82005 <zton82005@gmail.com>
2023-07-24 01:07:08 +0800

    Updated Client code

--- app/page.tsx
+++ app/page.tsx
@@ -81,7 +81,7 @@ export default function Home() {
               {loading && (
                 <div className='mt-16 w-full flex-center'>
                   <Image 
-                    src='/loader.svg'
+                    src='/loader.png'
                     alt='loader'
                     width={50}
                     height={50}
